b944c72ea04e2b3b1899cca97913cb69d8e6be99
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-08-28 15:13:38 +0700

    add complete Login Screen

---
+++ screens/LoginScreen/index.js
@@ -0,0 +1,52 @@
+import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity, Button, Alert } from 'react-native'
+import React, { useContext } from 'react'
+import styles from './styles'
+import { useState } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import Spinner from 'react-native-loading-spinner-overlay'
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const {isLoading, login} = useContext(AuthContext);
+  // const [btn, onBtnPressed] = useState('')
+  return (
+    <SafeAreaView style={styles.container}>
+      <Spinner visible={isLoading} /> 
+      <Image
+        source={require('../../assets/MonkeyIcon.jpeg')}
+        style={styles.image}
+      />
+      <Text style={styles.header}>Login with an existing account</Text>
+
+      <TextInput
+        style={styles.textInput}
+        placeholder='Email...'
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        style={styles.textInput}
+        placeholder='Password...'
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <Button
+        title='Login'
+        onPress={() => { login(email, password) }}
+      // onPress={() => console.log(email)}
+      />
+      <View style={styles.registerContainter}>
+        <Text style={styles.registerText}>Don't have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('register')}>
+           <Text style={styles.registerLink}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+export default LoginScreen
