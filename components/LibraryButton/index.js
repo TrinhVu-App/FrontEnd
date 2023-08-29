48d2bf9968a6a6ea3a5af2e7ce1a36bacc7c69f6
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-08-29 09:44:50 +0700

    update libraryButton with navigation function

--- components/LibraryButton/index.js
+++ components/LibraryButton/index.js
@@ -1,17 +1,22 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from './styles'
 
-const LibraryButton = () => {
+const LibraryButton = ({ navigation }) => {
+  
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        navigation.navigate('library');
+      }}>
         <View style={styles.textZone}>
             <Text style={styles.buttonText}>Library</Text>
             <Text style={styles.flavorText}>Khám phá tự do</Text>
         </View>
         
         <View style={styles.imageContainer}>
-            <Image 
+            <Image
                 style={styles.image}
                 source={require('../../assets/monkeyHead.png')}
             />
