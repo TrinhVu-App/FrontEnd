b25cdbe9689003536bad0a8bbf2be0a7b0e9cb06
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-09-06 15:43:07 +0700

    update download button to start button

--- components/StartButton/index.js
+++ components/StartButton/index.js
@@ -2,21 +2,21 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { faCircle } from '@fortawesome/free-regular-svg-icons'
 
-const DownloadButton = () => {
+const StartButton = () => {
+    
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.buttonContainer}>
                 <FontAwesomeIcon icon={faCircle} style={{ color: "#ffffff", position: 'absolute' }} size={135} />
-                <FontAwesomeIcon icon={faDownload} style={{ color: "#ffffff" }} size={30} />
-                <Text style={styles.progressText}>3.22 MB</Text>
+                <FontAwesomeIcon icon={faPlay} color='#fff' size={60}/>
             </TouchableOpacity>
-            <Text style={styles.downloadText}>Download</Text>
+            <Text style={styles.downloadText}>Start</Text>
         </View>
 
     )
 }
 
-export default DownloadButton
+export default StartButton
