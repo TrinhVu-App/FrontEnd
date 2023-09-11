5cf0a998200e00b580f2fb7e82840c2a104a6166
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-09-11 17:28:39 +0700

    add DEMo_DATA

---
+++ DEMO_DATA.js
@@ -0,0 +1,178 @@
+export const AUDIO_RESOURCE = {
+    1: require('./resource/audios/apple.mp3'),
+    2: require('./resource/audios/boiling-water.mp3'),
+    3: require('./resource/audios/bowl.mp3'),
+    4: require('./resource/audios/boy.mp3'),
+    5: require('./resource/audios/carrot.mp3'),
+    6: require('./resource/audios/cheese-grater.mp3'),
+    7: require('./resource/audios/cheese.mp3'),
+    8: require('./resource/audios/chopping-board.mp3'),
+    9: require('./resource/audios/cooking-pot.mp3'),
+    10: require('./resource/audios/cucumber.mp3'),
+    11: require('./resource/audios/Don-t-forget-to-add-some-cheese!.mp3'),
+    12: require('./resource/audios/dressing.mp3'),
+    13: require('./resource/audios/egg.mp3'),
+    14: require('./resource/audios/Finally-I-will-add-the-dressing..mp3'),
+    15: require('./resource/audios/fork.mp3'),
+    16: require('./resource/audios/frying-pan.mp3'),
+    17: require('./resource/audios/gas-stove.mp3'),
+    18: require('./resource/audios/Hey-do-you-want-to-eat-some-salads.mp3'),
+    19: require('./resource/audios/I-will-boil-the-eggs-and-fry-the-pork.mp3'),
+    20: require('./resource/audios/I-will-shred-some-lettuce.mp3'),
+    21: require('./resource/audios/It-s-fun-to-make-something-together.-Now-let-s-enjoy!.mp3'),
+    22: require('./resource/audios/knife.mp3'),
+    23: require('./resource/audios/Let-s-make-a-salad-bowl.mp3'),
+    24: require('./resource/audios/lettuce.mp3'),
+    25: require('./resource/audios/Now-we-need-to-mix-them-together.mp3'),
+    26: require('./resource/audios/Of-course-we-need-some-fruits-and-vegetables..mp3'),
+    27: require('./resource/audios/pineapple.mp3'),
+    28: require('./resource/audios/pork.mp3'),
+    29: require('./resource/audios/salad-bowl.mp3'),
+    30: require('./resource/audios/sink.mp3'),
+    31: require('./resource/audios/Sounds-great.mp3'),
+    32: require('./resource/audios/spatula.mp3'),
+    33: require('./resource/audios/spoon.mp3'),
+    34: require('./resource/audios/tap.mp3'),
+    35: require('./resource/audios/The-salad-bowl-looks-so-delicious!.mp3')
+}
+
+export const IMAGE_RESOURCE = {
+    1: require('./resource/images/Lg4oU6Aq4DxsWdwLByyCax1672904703767_trong.png'),
+    2: require('./resource/images/uQx8vLnhByvU8BGfJnpxur1672904703648_trong.png'),
+    3: require('./resource/images/j2FtITkWXWvEA2eeiolqNH1672904703836_trong.png'),
+    4: require('./resource/images/sK2dF8gy6Ou6nAWXbfmQgL1672904703791_trong.png'),
+    5: require('./resource/images/vcxJRLt9ebNd5KPtHWw1Gl1672904703709_trong.png'),
+    6: require('./resource/images/mTslr1cBESgjHOKfuCbXVL1672904703734_trong.png'),
+    7: require('./resource/images/m7ONzmMXmQkaJ8mj3jrSdI1672904703799_trong.png'),
+    8: require('./resource/images/ZWSAJtLuPueiEa9JpFPyqy1672904703749_trong.png'),
+    9: require('./resource/images/tyBrNbK7J3RQNyp5iFyOgk1672904703758_trong.png'),
+    10: require('./resource/images/ONwVWDqBvuLzO9JRN2rmTH1672904703776_trong.png')
+}
+
+
+export const DEMO_PAGE_DATA_1 = {
+    ID: 1,
+    background: 1,
+    title: "Hey, do you want to eat some salads?",
+    titleAudio: 18,
+    sync_data: [
+        {
+            "e": 900,
+            "s": 0,
+            "te": 3,
+            "ts": 0,
+            "w": "Hey,"
+        },
+        {
+            "e": 1090,
+            "s": 900,
+            "te": 6,
+            "ts": 5,
+            "w": "do"
+        },
+        {
+            "e": 1300,
+            "s": 1090,
+            "te": 10,
+            "ts": 8,
+            "w": "you"
+        },
+        {
+            "e": 1660,
+            "s": 1300,
+            "te": 15,
+            "ts": 12,
+            "w": "want"
+        },
+        {
+            "e": 1850,
+            "s": 1660,
+            "te": 18,
+            "ts": 17,
+            "w": "to"
+        },
+        {
+            "e": 2100,
+            "s": 1850,
+            "te": 22,
+            "ts": 20,
+            "w": "eat"
+        },
+        {
+            "e": 2450,
+            "s": 2100,
+            "te": 27,
+            "ts": 24,
+            "w": "some"
+        },
+        {
+            "e": 3530,
+            "s": 2450,
+            "te": 35,
+            "ts": 29,
+            "w": "salads?"
+        }
+    ],
+    titleAudio: 18,
+    touchables: [
+        {
+            name: "Salad bowl",
+            audio: 29,
+            position: {
+                x: 355,
+                y: 130
+            },
+            width: 95,
+            height: 70
+        },
+        {
+            name: "Boy",
+            audio: 4,
+            position: {
+                x: 275,
+                y: 280
+            },
+            width: 70,
+            height: 100
+        },
+        {
+            name: "Boy",
+            audio: 4,
+            position: {
+                x: 480,
+                y: 280
+            },
+            width: 70,
+            height: 100
+        },
+        {
+            name: "Fork",
+            audio: 15,
+            position: {
+                x: 425,
+                y: 206
+            },
+            width: 59,
+            height: 6
+        },
+        {
+            name: "Spoon",
+            audio: 33,
+            position: {
+                x: 326,
+                y: 196
+            },
+            width: 46,
+            height: 29
+        }
+    ]
+}
+
+export const DEMO_STORY_DATA = {
+    ID: 420,
+    storyName: "Let's Make A Salad Bowl!",
+    level: "A",
+    author: "Hilary Tran",
+    illustrator: "Nathan Pham",
+    pages: [DEMO_PAGE_DATA_1]
+}
