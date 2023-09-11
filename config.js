ae0cc7e00ee45b7c7e85510484c292773af65c64
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-09-11 17:30:32 +0700

    add some static resource path

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://1999-222-252-17-100.ngrok-free.app'
+export const BASE_URL = 'https://2a6a-222-252-17-100.ngrok-free.app'
+export const RESOURCE_PATH = '../../resource/4057_1_4307_1688701526'
+export const SourceSansPro = require("./assets/font/SourceSansPro-Regular.ttf")
