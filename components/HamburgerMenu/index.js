da1d3272b14fc5e965a6ef977c6a03a5d4cda882
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-09-22 16:40:41 +0700

    update HamburgerMenu: add function to display the menu

--- components/HamburgerMenu/index.js
+++ components/HamburgerMenu/index.js
@@ -4,9 +4,16 @@ import styles from './styles'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const HambergerMenu = () => {
+const HambergerMenu = ({setIsShowingMenu, isShowingMenu}) => {
+    const menuToggler = () => {
+        if (!isShowingMenu) {
+            setIsShowingMenu(true);
+        } else {
+            setIsShowingMenu(false)
+        }
+    }
     return (
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity style={styles.container} onPress={menuToggler}>
            <FontAwesomeIcon icon={faBars} style={{color: "#07BCEB"}} size={25} />
         </TouchableOpacity>
     )
