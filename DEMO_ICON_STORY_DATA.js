c71a89841db6b61641c8dca5e116f4a73a298cb9
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-10-13 16:00:06 +0700

    update demo datas

--- DEMO_ICON_STORY_DATA.js
+++ DEMO_ICON_STORY_DATA.js
@@ -97,7 +97,7 @@ const DEMO_PAGE_DATA_1 = {
                 x: 348,
                 y: 101
             },
-            width: 252,
+            width: 70,
             height: 85
         },
         {
@@ -396,10 +396,10 @@ const DEMO_PAGE_DATA_3 = {
     type: 1, //icon page
     background: 3,
     backgroundConfig: {
-        x: 0.1,
-        y: 0.2,
+        x: 0.15,
+        y: 0.75,
         w: 0.3,
-        h: 0.3
+        h: 0.25
     },
     title: ['Roy found many strange plants, flowers, and trees in the jungle. With his magnifying glass, he found mushrooms. One was brown, one red with spots, and one was tall and white.'],
     titleAudio: [10],
@@ -676,10 +676,10 @@ const DEMO_PAGE_DATA_4 = {
     type: 1,
     background: 4,
     backgroundConfig: {
-        x: 0.1,
-        y: 0.2,
-        w: 0.3,
-        h: 0.3
+        x: 0.25,
+        y: 0.6,
+        w: 0.5,
+        h: 0.5
     },
     title: ['Milo was chasing a rabbit, but he stopped to sniff the mushrooms with his nose. He wagged his tail because he was hungry and thought the mushrooms would be a good snack.'],
     titleAudioDuration: [13714],
@@ -960,6 +960,597 @@ const DEMO_PAGE_DATA_4 = {
     ]
 
 }
+
+const DEMO_PAGE_DATA_5 = {
+    ID: 4,
+    type: 1,
+    background: 5,
+    backgroundConfig: {
+        x: 0.5,
+        y: 0.7,
+        w: 0.3,
+        h: 0.3
+    },
+    title: ["“No!” cried Roy. “Don’t eat those mushrooms! They can make you sick!” Milo stopped just in time. “But I am hungry,” he whined. “Do you have some food in your pack?”"],
+    titleAudioDuration: [16848],
+    titleAudio: [8],
+    sync_data: [
+        {
+            "e": 1130,
+            "s": 0,
+            "te": 4,
+            "ts": 0,
+            "i": 0,
+            "w": "“No!”"
+        },
+        {
+            "e": 1660,
+            "s": 1130,
+            "te": 10,
+            "ts": 6,
+            "i": 0,
+            "w": "cried"
+        },
+        {
+            "e": 2570,
+            "s": 1660,
+            "te": 15,
+            "ts": 12,
+            "i": 1,
+            "w": "Roy."
+        },
+        {
+            "e": 3330,
+            "s": 2570,
+            "te": 22,
+            "ts": 17,
+            "i": 0,
+            "w": "“Don’t"
+        },
+        {
+            "e": 3600,
+            "s": 3330,
+            "te": 26,
+            "ts": 24,
+            "i": 0,
+            "w": "eat"
+        },
+        {
+            "e": 3920,
+            "s": 3600,
+            "te": 32,
+            "ts": 28,
+            "i": 0,
+            "w": "those"
+        },
+        {
+            "e": 4960,
+            "s": 3920,
+            "te": 43,
+            "ts": 34,
+            "i": 1,
+            "w": "mushrooms!"
+        },
+        {
+            "e": 5310,
+            "s": 4960,
+            "te": 48,
+            "ts": 45,
+            "i": 0,
+            "w": "They"
+        },
+        {
+            "e": 5510,
+            "s": 5310,
+            "te": 52,
+            "ts": 50,
+            "i": 0,
+            "w": "can"
+        },
+        {
+            "e": 5770,
+            "s": 5510,
+            "te": 57,
+            "ts": 54,
+            "i": 0,
+            "w": "make"
+        },
+        {
+            "e": 6010,
+            "s": 5770,
+            "te": 61,
+            "ts": 59,
+            "i": 0,
+            "w": "you"
+        },
+        {
+            "e": 6920,
+            "s": 6010,
+            "te": 68,
+            "ts": 63,
+            "i": 0,
+            "w": "sick!”"
+        },
+        {
+            "e": 7920,
+            "s": 6920,
+            "te": 73,
+            "ts": 70,
+            "i": 1,
+            "w": "Milo"
+        },
+        {
+            "e": 8640,
+            "s": 7920,
+            "te": 81,
+            "ts": 75,
+            "i": 0,
+            "w": "stopped"
+        },
+        {
+            "e": 8980,
+            "s": 8640,
+            "te": 86,
+            "ts": 83,
+            "i": 0,
+            "w": "just"
+        },
+        {
+            "e": 9260,
+            "s": 8980,
+            "te": 89,
+            "ts": 88,
+            "i": 0,
+            "w": "in"
+        },
+        {
+            "e": 10140,
+            "s": 9260,
+            "te": 95,
+            "ts": 91,
+            "i": 0,
+            "w": "time."
+        },
+        {
+            "e": 10590,
+            "s": 10140,
+            "te": 100,
+            "ts": 97,
+            "i": 0,
+            "w": "“But"
+        },
+        {
+            "e": 10730,
+            "s": 10590,
+            "te": 102,
+            "ts": 102,
+            "i": 0,
+            "w": "I"
+        },
+        {
+            "e": 11050,
+            "s": 10730,
+            "te": 105,
+            "i": 0,
+            "ts": 104,
+            "w": "am"
+        },
+        {
+            "e": 12250,
+            "s": 11050,
+            "te": 114,
+            "ts": 107,
+            "i": 0,
+            "w": "hungry,”"
+        },
+        {
+            "e": 12740,
+            "s": 12250,
+            "te": 117,
+            "ts": 116,
+            "i": 0,
+            "w": "he"
+        },
+        {
+            "e": 13770,
+            "s": 12740,
+            "te": 125,
+            "ts": 119,
+            "i": 0,
+            "w": "whined."
+        },
+        {
+            "e": 14430,
+            "s": 13770,
+            "te": 129,
+            "ts": 127,
+            "i": 0,
+            "w": "“Do"
+        },
+        {
+            "e": 14650,
+            "s": 14430,
+            "te": 133,
+            "i": 0,
+            "ts": 131,
+            "w": "you"
+        },
+        {
+            "e": 14930,
+            "s": 14650,
+            "te": 138,
+            "i": 0,
+            "ts": 135,
+            "w": "have"
+        },
+        {
+            "e": 15310,
+            "s": 14930,
+            "i": 0,
+            "te": 143,
+            "ts": 140,
+            "w": "some"
+        },
+        {
+            "e": 15690,
+            "i": 0,
+            "s": 15310,
+            "te": 148,
+            "ts": 145,
+            "w": "food"
+        },
+        {
+            "e": 15860,
+            "s": 15690,
+            "te": 151,
+            "i": 0,
+            "ts": 150,
+            "w": "in"
+        },
+        {
+            "e": 16060,
+            "s": 15860,
+            "i": 0,
+            "te": 156,
+            "ts": 153,
+            "w": "your"
+        },
+        {
+            "e": 17680,
+            "i": 0,
+            "s": 16060,
+            "te": 163,
+            "ts": 158,
+            "w": "pack?”"
+        }
+    ],
+    icons: [
+        {
+            name: "Mushrooms",
+            width: 69,
+            height: 46,
+            imageID: 11,
+            audioID: 7
+        },
+        {
+            name: "Milo",
+            width: 44,
+            height: 65,
+            imageID: 10,
+            audioID: 6
+        },
+        {
+            name: "Roy",
+            width: 26,
+            height: 65,
+            imageID: 12,
+            audioID: 11
+        },
+    ]
+}
+
+const DEMO_PAGE_DATA_6 = {
+    ID: 5,
+    type: 1,
+    background: 6,
+    backgroundConfig: {
+        x: 0.15,
+        y: 0.75,
+        w: 0.3,
+        h: 0.25
+    },
+    title: ["“Sure!” said Roy with a smile. “Mom made us both some hamburgers!” Roy took one from his pack and gave it to Milo. “This is much better than bad mushrooms!” Milo barked"],
+    titleAudio: [12],
+    titleAudioDuration: [16457],
+    sync_data: [
+        {
+            "e": 1180,
+            "s": 0,
+            "te": 6,
+            "ts": 0,
+            "i": 0,
+            "w": "“Sure!”"
+        },
+        {
+            "e": 1810,
+            "s": 1180,
+            "te": 11,
+            "ts": 8,
+            "i": 0,
+            "w": "said"
+        },
+        {
+            "e": 2500,
+            "s": 1810,
+            "te": 15,
+            "i": 1,
+            "ts": 13,
+            "w": "Roy"
+        },
+        {
+            "e": 2800,
+            "s": 2500,
+            "te": 20,
+            "i": 0,
+            "ts": 17,
+            "w": "with"
+        },
+        {
+            "e": 2970,
+            "s": 2800,
+            "te": 22,
+            "i": 0,
+            "ts": 22,
+            "w": "a"
+        },
+        {
+            "e": 3870,
+            "s": 2970,
+            "i": 0,
+            "te": 29,
+            "ts": 24,
+            "w": "smile."
+        },
+        {
+            "e": 4560,
+            "s": 3870,
+            "te": 34,
+            "i": 0,
+            "ts": 31,
+            "w": "“Mom"
+        },
+        {
+            "e": 4940,
+            "s": 4560,
+            "i": 0,
+            "te": 39,
+            "ts": 36,
+            "w": "made"
+        },
+        {
+            "e": 5210,
+            "i": 0,
+            "s": 4940,
+            "te": 42,
+            "ts": 41,
+            "w": "us"
+        },
+        {
+            "e": 5620,
+            "s": 5210,
+            "te": 47,
+            "i": 0,
+            "ts": 44,
+            "w": "both"
+        },
+        {
+            "e": 5920,
+            "s": 5620,
+            "te": 52,
+            "i": 0,
+            "ts": 49,
+            "w": "some"
+        },
+        {
+            "e": 7500,
+            "s": 5920,
+            "i": 1,
+            "te": 65,
+            "ts": 54,
+            "w": "hamburgers!”"
+        },
+        {
+            "e": 8160,
+            "s": 7500,
+            "te": 69,
+            "ts": 67,
+            "i": 1,
+            "w": "Roy"
+        },
+        {
+            "e": 8350,
+            "s": 8160,
+            "te": 74,
+            "i": 0,
+            "ts": 71,
+            "w": "took"
+        },
+        {
+            "e": 8720,
+            "i": 0,
+            "s": 8350,
+            "te": 78,
+            "ts": 76,
+            "w": "one"
+        },
+        {
+            "e": 8870,
+            "s": 8720,
+            "te": 83,
+            "ts": 80,
+            "i": 0,
+            "w": "from"
+        },
+        {
+            "e": 9190,
+            "s": 8870,
+            "i": 0,
+            "te": 87,
+            "ts": 85,
+            "w": "his"
+        },
+        {
+            "e": 9610,
+            "i": 0,
+            "s": 9190,
+            "te": 92,
+            "ts": 89,
+            "w": "pack"
+        },
+        {
+            "e": 9920,
+            "s": 9610,
+            "te": 96,
+            "i": 0,
+            "ts": 94,
+            "w": "and"
+        },
+        {
+            "e": 10120,
+            "s": 9920,
+            "i": 0,
+            "te": 101,
+            "ts": 98,
+            "w": "gave"
+        },
+        {
+            "e": 10290,
+            "s": 10120,
+            "i": 0,
+            "te": 104,
+            "ts": 103,
+            "w": "it"
+        },
+        {
+            "e": 10440,
+            "s": 10290,
+            "i": 0,
+            "te": 107,
+            "ts": 106,
+            "w": "to"
+        },
+        {
+            "e": 11580,
+            "s": 10440,
+            "i": 1,
+            "te": 113,
+            "ts": 109,
+            "w": "Milo."
+        },
+        {
+            "e": 12150,
+            "s": 11580,
+            "i": 0,
+            "te": 119,
+            "ts": 115,
+            "w": "“This"
+        },
+        {
+            "e": 12370,
+            "s": 12150,
+            "i": 0,
+            "te": 122,
+            "ts": 121,
+            "w": "is"
+        },
+        {
+            "e": 12920,
+            "s": 12370,
+            "te": 127,
+            "ts": 124,
+            "i": 0,
+            "w": "much"
+        },
+        {
+            "e": 13260,
+            "s": 12920,
+            "te": 134,
+            "i": 0,
+            "ts": 129,
+            "w": "better"
+        },
+        {
+            "e": 13560,
+            "i": 0,
+            "s": 13260,
+            "te": 139,
+            "ts": 136,
+            "w": "than"
+        },
+        {
+            "e": 13880,
+            "s": 13560,
+            "i": 0,
+            "te": 143,
+            "ts": 141,
+            "w": "bad"
+        },
+        {
+            "e": 15010,
+            "s": 13880,
+            "te": 155,
+            "ts": 145,
+            "i": 1,
+            "w": "mushrooms!”"
+        },
+        {
+            "e": 15590,
+            "s": 15010,
+            "te": 160,
+            "ts": 157,
+            "i": 1,
+            "w": "Milo"
+        },
+        {
+            "e": 16700,
+            "s": 15590,
+            "i": 0,
+            "te": 168,
+            "ts": 162,
+            "w": "barked."
+        }
+    ],
+    icons: [
+        {
+            name: "Mushrooms",
+            width: 69,
+            height: 46,
+            imageID: 11,
+            audioID: 7
+        },
+        {
+            name: "Milo",
+            width: 44,
+            height: 65,
+            imageID: 10,
+            audioID: 6
+        },
+        {
+            name: "Roy",
+            width: 26,
+            height: 65,
+            imageID: 12,
+            audioID: 11
+        },
+        {
+            name: "Hamburgers",
+            width: 60,
+            height: 30,
+            imageID: 8,
+            audioID: 3
+        },
+    ]
+}
 export const DEMO_ICON_STORY = {
     id: 421,
     storyName: "Danger in the Jungle",
@@ -971,6 +1562,8 @@ export const DEMO_ICON_STORY = {
         DEMO_PAGE_DATA_1,
         DEMO_PAGE_DATA_2,
         DEMO_PAGE_DATA_3,
-        DEMO_PAGE_DATA_4
+        DEMO_PAGE_DATA_4,
+        DEMO_PAGE_DATA_5,
+        DEMO_PAGE_DATA_6
     ]
 }
