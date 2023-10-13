f1f7c40e7d9d83d992dc11b24d968424c81584ea
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-10-13 16:01:35 +0700

    minor config tweaks

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { Dimensions } from "react-native";
 
-const BASE_WIDTH = 683.4285714285714;
-const BASE_HEIGHT = 411.42857142857144;
+export const BASE_WIDTH = 683.4285714285714;
+export const BASE_HEIGHT = 411.42857142857144;
 
 export const screenWidth = Dimensions.get("window").width;
 export const screenHeight = Dimensions.get("window").height;
@@ -15,7 +15,6 @@ console.log("Screen height: " + screenHeight)
 console.log("WIDTH SCALE: " + WIDTH_SCALE);
 console.log("HEIGHT SCALE: "+ HEIGHT_SCALE);
 
-
-export const BASE_URL = 'https://2a6a-222-252-17-100.ngrok-free.app'
+export const BASE_URL = 'https://7473-14-232-245-96.ngrok-free.app'
 export const RESOURCE_PATH = '../../resource/4057_1_4307_1688701526'
 export const SourceSansPro = require("./assets/font/SourceSansPro-Regular.ttf")
