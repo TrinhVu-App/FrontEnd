7fb2802309eaa7b2944d595cbdf2cbdcb705b387
VuDoiMuLap <trinhlongvu153@gmail.com>
2023-10-20 16:11:17 +0700

    update Icon: minor bugs on synctext start

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import { ICON_STORY_AUDIO_RESOURCE, ICON_STORY_IMAGE_RESOURCE } from '../../DEMO_ICON_STORY_DATA'
 // import { Group, Image, Text, useFont, useImage, useSpring, useValue } from '@shopify/react-native-skia'
-import Animated, { useDerivedValue, useSharedValue, withSpring } from 'react-native-reanimated'
+import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withRepeat, withSequence, withSpring, withTiming } from 'react-native-reanimated'
 import { Audio } from 'expo-av'
 // { iconX, iconY, iconH, iconW, font, text, isHighlight, image, isToggle }
 
@@ -37,48 +37,59 @@ const Icon = ({ imageID, text, w, h, highlight, word, audioID, isSyncText}) => {
 
   const image = ICON_STORY_IMAGE_RESOURCE[imageID];
 
-  const width = useSharedValue(w);
-  const height = useSharedValue(h);
+  const offset = useSharedValue(0);
+  const scale = useSharedValue(1);
+
+	const animateStyle = useAnimatedStyle(() => ({
+		transform: [{ translateX: offset.value }, {scale: scale.value}],
+	}));
+
+	const OFFSET = 2;
+	const TIME = 50;
 
   const touchHandler = () => {
     if(!isSyncText) {
     if (!toggle) {
       playSound(audio);
-      width.value = withSpring(width.value * 0.7)
-      height.value = withSpring(height.value * 0.7)
+      scale.value = withSpring(0.7)
       setToggle(!toggle)
     } else {
-      width.value = withSpring(w)
-      height.value = withSpring(h)
+      scale.value = withSpring(1)
       setToggle(!toggle)
     }
     } else {
       console.log("Disabled until synctext is done!");
+      offset.value = withSequence(
+        withTiming(-OFFSET, { duration: TIME / 2 }),
+        withRepeat(withTiming(OFFSET, { duration: TIME }), 3, true),
+        withTiming(0, { duration: TIME / 2 })
+      );
     }
     
   }
 
+  useEffect(() => {
+      setToggle(false)
+      scale.value = withSpring(1)
+
+  }, [isSyncText])
+
 
   if (highlight) {
-    width.value = withSpring(width.value * 1.3);
-    height.value = withSpring(height.value * 1.3);
-
+    scale.value = withSpring(1.3)
     setTimeout(() => {
-      width.value = withSpring(w);
-      height.value = withSpring(h);
+      scale.value = withSpring(1)
     }, 300)
   }
 
 
   return (
     <View style={[styles.container, { width: (hasSymbol? (w+ 20) : (w)), height: (h + 22) }]}>
-      <Pressable style={[styles.image, { width: (w+ 5), height: (h + 22)}]} onPress={touchHandler}>
+      <Pressable style={[styles.image, {width: w, height: h}]} onPress={touchHandler}>
         <Animated.Image
-        style={{width:w, height: h, }}
+        style={[{width: w, height: h}, animateStyle]}
           source={image}
           resizeMode={'contain'}
-          width={width}
-          height={height}
         />
       </Pressable>
       {toggle && (
